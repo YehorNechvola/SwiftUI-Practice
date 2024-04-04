1cf0f4fa9918cec35575a3d95e8674c5af422724
Yehor Nechvola <ynechvola@eva.ua>
2024-04-04 17:49:24 +0300

    added empty xcode project to git flder

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Eva on 04.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
